732e3dbe316da645bfd627c441d24691155c4d44
JoaoVLira <joao.alves@sptech.school>
2026-03-31 17:53:59 -0300

    Update script_sax_V2.sql

--- script_sax_V2.sql
+++ script_sax_V2.sql
@@ -35,7 +35,7 @@ CREATE TABLE fornecedor (
     CONSTRAINT fk_fornecedor_tipo FOREIGN KEY (tipo_fornecedor_id) REFERENCES tipo_fornecedor(id)
 );
 
--- estoque (limites, almoxarifado e etc...)
+-- almoxarifado e limites
 
 CREATE TABLE tipo_limite (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -49,17 +49,11 @@ CREATE TABLE limites (
     CONSTRAINT fk_limites_tipo FOREIGN KEY (tipo_limite_id) REFERENCES tipo_limite(id)
 );
 
-CREATE TABLE estoque (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    limites_id INT,
-    CONSTRAINT fk_estoque_limites FOREIGN KEY (limites_id) REFERENCES limites(id)
-);
-
 CREATE TABLE almoxarifado (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    estoque_id INT,
+    limites_id INT,
     numero_sala INT,
-    CONSTRAINT fk_almoxarifado_estoque FOREIGN KEY (estoque_id) REFERENCES estoque(id)
+    CONSTRAINT fk_almoxarifado_limites FOREIGN KEY (limites_id) REFERENCES limites(id)
 );
 
 CREATE TABLE almoxarife (
@@ -68,6 +62,7 @@ CREATE TABLE almoxarife (
     nome VARCHAR(45),
     email VARCHAR(45),
     telefone VARCHAR(45),
+    senha VARCHAR(16),
     CONSTRAINT fk_almoxarife_almoxarifado FOREIGN KEY (almoxarifado_id) REFERENCES almoxarifado(id)
 );
 
@@ -86,44 +81,26 @@ CREATE TABLE unidade_medida (
 CREATE TABLE material (
     id INT AUTO_INCREMENT PRIMARY KEY,
     categoria_id INT,
-    estoque_id INT,
+    almoxarifado_id INT,
     nome_material VARCHAR(45),
     unidade_medida_id INT,
+    quantidade INT,
     CONSTRAINT fk_material_categoria FOREIGN KEY (categoria_id) REFERENCES categoria(id),
-    CONSTRAINT fk_material_estoque FOREIGN KEY (estoque_id) REFERENCES estoque(id),
+    CONSTRAINT fk_material_almoxarifado FOREIGN KEY (almoxarifado_id) REFERENCES almoxarifado(id),
     CONSTRAINT fk_material_unidade FOREIGN KEY (unidade_medida_id) REFERENCES unidade_medida(id)
 );
 
 -- Entradas e Saídas
 
--- históricos
-CREATE TABLE historico_entrada (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    data_entrada DATE,
-    nome_responsavel VARCHAR(45),
-    quantidade INT,
-    data_registrada DATE
-);
-
-CREATE TABLE historico_saida (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    data_saida DATE,
-    nome_solicitante VARCHAR(45),
-    escala_saida VARCHAR(45),
-    quantidade INT
-);
-
 -- tabelas de pedido (relacionamentos muitos para muitos) e escala da saída
 CREATE TABLE pedido_entrada (
     fornecedor_id INT,
-    material_id INT,
-    historico_entrada_id INT,
+    material_id INT
     quantidade INT,
     data_entrada DATE,
     PRIMARY KEY (fornecedor_id, material_id, historico_entrada_id),
     CONSTRAINT fk_ped_ent_fornecedor FOREIGN KEY (fornecedor_id) REFERENCES fornecedor(id),
-    CONSTRAINT fk_ped_ent_material FOREIGN KEY (material_id) REFERENCES material(id),
-    CONSTRAINT fk_ped_ent_hist FOREIGN KEY (historico_entrada_id) REFERENCES historico_entrada(id)
+    CONSTRAINT fk_ped_ent_material FOREIGN KEY (material_id) REFERENCES material(id)
 );
 
 CREATE TABLE escala (
@@ -134,13 +111,11 @@ CREATE TABLE escala (
 CREATE TABLE pedido_saida (
     material_id INT,
     solicitacao_id INT,
-    historico_saida_id INT,
     escala_id INT,
     quantidade INT,
     data_solicitacao DATE,
     PRIMARY KEY (material_id, solicitacao_id),
     CONSTRAINT fk_ped_sai_material FOREIGN KEY (material_id) REFERENCES material(id),
     CONSTRAINT fk_ped_sai_solicitacao FOREIGN KEY (solicitacao_id) REFERENCES solicitacao(id),
-    CONSTRAINT fk_ped_sai_hist FOREIGN KEY (historico_saida_id) REFERENCES historico_saida(id),
     CONSTRAINT fk_ped_sai_escala FOREIGN KEY (escala_id) REFERENCES escala(id)
 );
